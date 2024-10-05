9a91db09a7547fc78a9e0801e8bab5c29eba25be
Johnlman <johnlman1@yahoo.com>
2024-10-05 16:51:45 -0700

    Connected to MongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,29 @@ import CreateAccountForm from "./pages/CreateAccount.jsx";
 import SignInForm from "./pages/SignIn.jsx";
 import {Route, Routes} from "react-router-dom";
 import Status from './pages/Status.jsx';
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://johnlman1:FXm3h8qm1AllyzmE@stormhacks.i9q8l.mongodb.net/?retryWrites=true&w=majority&appName=Stormhacks";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 function App() {
     return (
         <>
