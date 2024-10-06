0d6983e65d48d3a53db131a7147e2a9985e3f670
NACK2 <nicholaskang5@gmail.com>
2024-10-05 19:23:27 -0700

    change to sunday

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -17,7 +17,7 @@ const Calendar = ({isPreferencesPage}) => {
         console.log(dayToRangesObj)
     }, [selectedCells])
 
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const timeSlots = [
         '0:00 - 1:00', '1:00 - 2:00', '2:00 - 3:00', '3:00 - 4:00', '4:00 - 5:00',
         '5:00 - 6:00', '6:00 - 7:00', '7:00 - 8:00', '8:00 - 9:00', '9:00 - 10:00',
